133c226a07cea5d068e65a0294a493b18b02883b
Angel Cancho <u201721995@upc.edu.pe>
2025-06-06 02:14:41 -0500

    feat: add endpoints for notifications by user type and user ID in AppConstants

--- lib/core/app_constants.dart
+++ lib/core/app_constants.dart
@@ -1,3 +1,7 @@
 class AppConstants {
   static const String baseUrl = 'http://10.0.2.2:8080/api/v1';
+
+  static const String notificationsByUserTypeEndpoint =
+      '/notifications/user-type';
+  static const String notificationsByUserIdEndpoint = '/notifications/user-id';
 }
