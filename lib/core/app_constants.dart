4d22f78f7ad1024bec12e8b8c44402f86aec4d9e
Angel Cancho <u201721995@upc.edu.pe>
2025-06-13 07:56:08 -0500

    feat: refactor notification endpoints to use a base path for improved maintainability

--- lib/core/app_constants.dart
+++ lib/core/app_constants.dart
@@ -1,7 +1,20 @@
 class AppConstants {
   static const String baseUrl = 'http://10.0.2.2:8080/api/v1';
 
+  static const String notificationsBase = '/notifications';
+
   static const String notificationsByUserTypeEndpoint =
-      '/notifications/user-type';
-  static const String notificationsByUserIdEndpoint = '/notifications/user-id';
+      '$notificationsBase/user-type';
+
+  static const String notificationsByUserIdEndpoint =
+      '$notificationsBase/user-id';
+
+  static const String notificationsByStudentIdEndpoint =
+      '$notificationsBase/student-id';
+
+  static const String notificationsByTripIdEndpoint =
+      '$notificationsBase/trip-id';
+
+  static const String notificationsByUserAndTripEndpoint =
+      '$notificationsBase/user-id/{userId}/trip-id/{tripId}';
 }
