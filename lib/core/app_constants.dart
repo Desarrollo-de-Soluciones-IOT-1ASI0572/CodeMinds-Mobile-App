396f1fd88888936af424242762971d47185a622d
Angel Cancho <u201721995@upc.edu.pe>
2025-06-16 03:17:33 -0500

    fix(app-constants): Correct baseUrl format by adding trailing slash

--- lib/core/app_constants.dart
+++ lib/core/app_constants.dart
@@ -1,3 +1,5 @@
 class AppConstants {
-  static const String baseUrl = 'http://10.0.2.2:8080/api/v1';
+  static const String baseUrl = 'http://10.0.2.2:8080/api/v1/';
+  //static const String baseUrl = 'http://localhost:8080/api/v1/';
+  static const String tripsEndpoint = 'vehicle-tracking/trips';
 }
