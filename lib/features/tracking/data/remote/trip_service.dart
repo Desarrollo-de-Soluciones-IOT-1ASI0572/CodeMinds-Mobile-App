2ff58126d8c2860b272b3e2e1521d243b73e58a0
Angel Cancho <u201721995@upc.edu.pe>
2025-06-16 03:17:19 -0500

    feat(trip-service): Add TripService for managing trip data retrieval and deletion

---
+++ lib/features/tracking/data/remote/trip_service.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:codeminds_mobile_application/features/tracking/data/remote/trip_dto.dart';
+import 'package:http/http.dart' as http;
+
+import 'package:codeminds_mobile_application/core/app_constants.dart';
+
+class TripService {
+  Future<List<TripDTO>> getAllTrips() async {
+    String url = '${AppConstants.baseUrl}${AppConstants.tripsEndpoint}';
+
+    try {
+      http.Response response = await http.get(Uri.parse(url));
+
+      if (response.statusCode == HttpStatus.ok) {
+        List<dynamic> jsonResponse = json.decode(response.body);
+        return jsonResponse.map((model) => TripDTO.fromJson(model)).toList();
+      }
+      return [];
+    } catch (e) {
+      return [];
+    }
+  }
+
+  Future<bool> deleteTrip(int tripId) async {
+    final url = '${AppConstants.baseUrl}${AppConstants.tripsEndpoint}/$tripId';
+
+    try {
+      final response = await http.delete(Uri.parse(url));
+
+      if (response.statusCode == HttpStatus.ok) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      return false;
+    }
+  }
+}
