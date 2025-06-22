74fcc1ed9ac61365a9ef1121b02a0b483d508cc2
paolomv02 <u202010039@upc.edu.pe>
2025-06-22 15:25:47 -0500

    feat(services-integration): add HTTP requests

--- lib/features/notification/data/remote/notification_service.dart
+++ lib/features/notification/data/remote/notification_service.dart
@@ -4,43 +4,91 @@ import 'dart:io';
 import 'package:codeminds_mobile_application/core/app_constants.dart';
 import 'package:codeminds_mobile_application/features/notification/data/remote/notification_dto.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class NotificationService {
   Future<List<NotificationDTO>> getNotificationsByUserType(
     String userType,
   ) async {
-    String url =
-        '${AppConstants.baseUrl}${AppConstants.notificationsByUserTypeEndpoint}/$userType';
-    try {
-      http.Response response = await http.get(Uri.parse(url));
+    String? token = await getToken();
+    http.Response response;
 
-      if (response.statusCode == HttpStatus.ok) {
-        List<dynamic> jsonResponse = json.decode(response.body);
-        return jsonResponse
-            .map((model) => NotificationDTO.fromJson(model))
-            .toList();
+    if (token != null) {
+      String url =
+          '${AppConstants.baseUrl}${AppConstants.notificationsByUserTypeEndpoint}/$userType';
+      try {
+        response = await http.get(
+          Uri.parse(url),
+          headers: {
+            'Authorization': 'Bearer $token',
+            'Content-Type': 'application/json',
+          },
+        );
+      } catch (e) {
+        return [];
       }
-      return [];
-    } catch (e) {
-      return [];
+    } else {
+      print('Token no encontrado. Por favor, inicia sesión.');
+      throw Exception('Token no encontrado');
+    }
+
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+
+    if (response.statusCode == HttpStatus.ok) {
+      List<dynamic> jsonResponse = json.decode(response.body);
+      return jsonResponse
+          .map((model) => NotificationDTO.fromJson(model))
+          .toList();
+    } else if (response.statusCode == HttpStatus.unauthorized) {
+      print('Error 401: No autorizado');
+      throw Exception('No autorizado');
+    } else {
+      throw Exception('Error al obtener las notificaciones');
     }
   }
 
   Future<List<NotificationDTO>> getNotificationsByUserId(int userId) async {
-    String url =
-        '${AppConstants.baseUrl}${AppConstants.notificationsByUserIdEndpoint}/$userId';
-    try {
-      http.Response response = await http.get(Uri.parse(url));
+    String? token = await getToken();
+    http.Response response;
 
-      if (response.statusCode == HttpStatus.ok) {
-        List<dynamic> jsonResponse = json.decode(response.body);
-        return jsonResponse
-            .map((model) => NotificationDTO.fromJson(model))
-            .toList();
+    if (token != null) {
+      String url =
+          '${AppConstants.baseUrl}${AppConstants.notificationsByUserIdEndpoint}/$userId';
+      try {
+        response = await http.get(
+          Uri.parse(url),
+          headers: {
+            'Authorization': 'Bearer $token',
+            'Content-Type': 'application/json',
+          },
+        );
+      } catch (e) {
+        return [];
       }
-      return [];
-    } catch (e) {
-      return [];
+    } else {
+      print('Token no encontrado. Por favor, inicia sesión.');
+      throw Exception('Token no encontrado');
     }
+
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+
+    if (response.statusCode == HttpStatus.ok) {
+      List<dynamic> jsonResponse = json.decode(response.body);
+      return jsonResponse
+          .map((model) => NotificationDTO.fromJson(model))
+          .toList();
+    } else if (response.statusCode == HttpStatus.unauthorized) {
+      print('Error 401: No autorizado');
+      throw Exception('No autorizado');
+    } else {
+      throw Exception('Error al obtener las notificaciones');
+    }
+  }
+
+  Future<String?> getToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString('jwt_token');
   }
 }
